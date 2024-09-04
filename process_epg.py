255cdfcc8be5df5194942989dae50afa068fc7b8
sadekmiah1 <76450770+sadekmiah1@users.noreply.github.com>
2024-09-04 19:32:08 +0600

    Update process_epg.py

--- process_epg.py
+++ process_epg.py
@@ -1,6 +1,6 @@
 import json
 
-def read_epg_file(file_path): 'https://raw.githubusercontent.com/sadekmiah1/all-in-one/main/epg.json'
+def read_epg_file(file_path): 
     """Reads and processes an EPG JSON file."""
     try:
         # Open and read the JSON file
